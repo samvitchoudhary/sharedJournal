c7f5c0a0c8714df9ccadfca3c39279fc7e2f2761
samvitchoudhary <samvitchoudh@gmail.com>
2026-03-16 16:23:34 -0400

    Initial Commit

---
+++ SharedJournal/SharedJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedJournalApp.swift
+//  SharedJournal
+//
+//  Created by Samvit Choudhary on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct SharedJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
